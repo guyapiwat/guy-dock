73b6b76ef0a878c6641bf8baa2d9816c8eaae726
pratch <pratch@dev.p-enterprise.com>
2018-08-24 12:15:33 +0700

    Update DockerFile

--- www/index.php
+++ www/index.php
@@ -5,6 +5,10 @@
  * Date: 21/8/2018 AD
  * Time: 00:01
  */
-echo('It\'s work')
-
+echo('It\'s work2');
+$test = '555666655';
+echo('<br/>');
+echo($test);
+echo('<br/>');
+echo (extension_loaded('xdebug') ? '' : 'non '), 'exists';
 ?>
