faeb5c5ef0d2b117a809237699b3e2973636b1d6
Saintent <Saintentex@gmail.com>
2018-09-06 23:06:34 +0700

    [Update db] Add autochip commission cal

--- www/public/member/connectmysql.php
+++ www/public/member/connectmysql.php
@@ -1,8 +1,8 @@
 <?php
 // connect to database
 $connectmysql_dbname = "cci_db";
-$link = @mysql_connect("localhost", "cci_db", "?okokok@omc12345") or die("Could not connect: " . mysql_error());
-//	$link = @mysql_connect("p-enterprise.com:9100", "dev", "dev@cci") or die("Could not connect: " . mysql_error());
+//$link = @mysql_connect("localhost", "cci_db", "?okokok@omc12345") or die("Could not connect: " . mysql_error());
+$link = @mysql_connect("p-enterprise.com:9100", "dev", "dev@cci") or die("Could not connect: " . mysql_error());
 
 //$charset = "SET NAMES 'tis620'";
 $charset = "SET NAMES 'UTF8'";
